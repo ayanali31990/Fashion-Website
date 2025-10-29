8a2c89c078e006a41d6cfd7c67d44e7513d212b0
ayan-ali <ayan31990@gmail.com>
2025-10-29 20:13:44 +0530

    sixth commit

--- src/pages/Agence.jsx
+++ src/pages/Agence.jsx
@@ -11,17 +11,7 @@ function Agence() {
 
 
   const imageArray = [
-    '1.jpg',
-    '2.jpg',
-    '3.jpg',
-    '4.jpg',
-    '5.jpg',
-    '6.jpg',
-    '7.jpg',
-    '8.jpg',
-    '9.jpg',
-    '10.jpg',
-    '11.jpg',
+    '1.jpg','2.jpg','3.jpg','4.jpg','5.jpg','6.jpg','7.jpg','8.jpg','9.jpg','10.jpg','11.jpg',
   ]
 
   useGSAP(function(){
