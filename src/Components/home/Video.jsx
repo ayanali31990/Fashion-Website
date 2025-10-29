8221765de44ad4cf172d5415323121f02352312d
ayan-ali <ayan31990@gmail.com>
2025-10-29 20:08:21 +0530

    fourth commit

--- src/Components/home/Video.jsx
+++ src/Components/home/Video.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Video() {
   return (
     <div className='h-full w-full'>
-        <video className='h-full w-full object-cover' autoPlay loop muted src="https://videos.pexels.com/video-files/2047965/2047965-hd_1280_720_30fps.mp4"></video>
+        <video className='h-full w-full object-cover' autoPlay loop muted src="69496b2d.mp4"></video>
     </div>
   )
 }
