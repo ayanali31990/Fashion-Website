b1d10889496e5803f7563d2db490c2a3369e9c65
ayan-ali <ayan31990@gmail.com>
2025-10-29 20:02:48 +0530

    second commit

--- src/Components/home/Video.jsx
+++ src/Components/home/Video.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Video() {
   return (
     <div className='h-full w-full'>
-        <video className='h-full w-full object-cover' autoPlay loop muted src="../../../public/69496b2d.mp4"></video>
+        <video className='h-full w-full object-cover' autoPlay loop muted src="https://www.pexels.com/video/a-billboard-in-the-city-2047965/"></video>
     </div>
   )
 }
